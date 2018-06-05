387a412e13a7be7dbd7fa1a58758fbf891ca4080
Bhavishya <bhavishyagopesh@gmail.com>
2018-06-05 09:46:25 +0800

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf public/
+
+git submodule add -b master git@github.com:meelgroup/meelgroup.github.io.git public
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
